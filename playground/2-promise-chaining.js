628c7f442fde220de8b40d79911d89884052e0d6
Rob Peckham <peckham.rob@gmail.com>
2021-02-15 16:42:09 -0500

    deleted a new id

--- playground/2-promise-chaining.js
+++ playground/2-promise-chaining.js
@@ -3,7 +3,7 @@ const Task = require('../src/models/task')
 
 // 60268ee1d7b9bcc5ceb3161c
 
-Task.findByIdAndDelete('60268ee1d7b9bcc5ceb3161c').then((task) => {
+Task.findByIdAndDelete('602ac9767df326ea0250e940').then((task) => {
   console.log(task)
   return Task.countDocuments({ completed: false })
 }).then((result) => {
