44b080aa019ea4d6831099a8a4097a2b9b375b45
Rob Peckham <peckham.rob@gmail.com>
2021-02-16 13:04:03 -0500

    refactored to use async await

--- playground/2-promise-chaining.js
+++ playground/2-promise-chaining.js
@@ -3,11 +3,20 @@ const Task = require('../src/models/task')
 
 // 60268ee1d7b9bcc5ceb3161c
 
-Task.findByIdAndDelete('602ac9767df326ea0250e940').then((task) => {
-  console.log(task)
-  return Task.countDocuments({ completed: false })
-}).then((result) => {
-  console.log(result)
-}).catch((e) => {
-  console.log(e)
-})
+// Task.findByIdAndDelete('602ac9767df326ea0250e940').then((task) => {
+//   console.log(task)
+//   return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//   console.log(result)
+// }).catch((e) => {
+//   console.log(e)
+// })
+
+const deleteTaskandCount = async (id) => {
+  await Task.findByIdAndDelete(id)
+  return await Task.countDocuments({ completed: false })
+}
+
+deleteTaskandCount('602ac8e509a16ae5e72d6323').then(count => {
+  console.log(count)
+}).catch(error => console.log(error))
