145d9851dd1f73bb7c983428546fde6f353d43a7
Rob Peckham <peckham.rob@gmail.com>
2021-02-16 12:52:42 -0500

    playing around with async/await

--- playground/1-promise-chaining.js
+++ playground/1-promise-chaining.js
@@ -4,11 +4,24 @@ const User = require('../src/models/user')
 
 // 6026ce6892e94d7be6b151d9
 
-User.findByIdAndUpdate('60269a65c2cb81eab2578b9b', { age: 1 }).then((user) => {
-  console.log(user)
-  return User.countDocuments({ age: 1 })
-}).then((result) => {
-  console.log(result)
-}).catch((e) => {
+// User.findByIdAndUpdate('60269a65c2cb81eab2578b9b', { age: 1 }).then((user) => {
+//   console.log(user)
+//   return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//   console.log(result)
+// }).catch((e) => {
+//   console.log(e)
+// })
+
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age })
+  return count 
+}
+
+updateAgeAndCount('60269a65c2cb81eab2578b9b', 2).then((count) => {
+  console.log(count)
+}).catch(e => {
   console.log(e)
 })
