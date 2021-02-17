a1359425c1c6e9b1bd07857ec7790a99a2d2820f
Rob Peckham <peckham.rob@gmail.com>
2021-02-17 08:27:33 -0500

    modified for use with mongoose middleware

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
@@ -13,3 +14,16 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`)
 })
+
+const myFunc = async () => {
+  const password = 'Red123!'
+  const hashedPassword = await bcrypt.hash(password, 8)
+
+  console.log(password)
+  console.log(hashedPassword)
+
+  const isMatch = await bcrypt.compare('Red123!', hashedPassword)
+  console.log(isMatch)
+}
+
+myFunc()
